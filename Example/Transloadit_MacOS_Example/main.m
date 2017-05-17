87526d574b9eb537e5d6ec1b6150cc3f7b7ba225
Mark R. Masterson <mark@Masterson.io>
2017-05-16 19:11:54 -0700

    MacOS Initial

---
+++ Example/Transloadit_MacOS_Example/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Transloadit_MacOS_Example
+//
+//  Created by Mark Masterson on 5/16/17.
+//  Copyright © 2017 Mark R. Masterson. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ Example/Transloadit_MacOS_ExampleTests/Transloadit_MacOS_ExampleTests.m
@@ -0,0 +1,39 @@
+//
+//  Transloadit_MacOS_ExampleTests.m
+//  Transloadit_MacOS_ExampleTests
+//
+//  Created by Mark Masterson on 5/16/17.
+//  Copyright © 2017 Mark R. Masterson. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Transloadit_MacOS_ExampleTests : XCTestCase
+
+@end
+
+@implementation Transloadit_MacOS_ExampleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
