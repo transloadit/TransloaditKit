c7982383d5ef70db055aeb9331e3fdaa4690a1f2
Mark R. Masterson <mark@Masterson.io>
2017-09-12 18:44:00 -0700

    Travis Tests

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -7,6 +7,7 @@
 //
 
 @import XCTest;
+@import TransloaditKit;
 
 @interface Tests : XCTestCase
 
@@ -18,7 +19,73 @@
 {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    
 }
+
+- (void)testResizeImageExistingFile
+{
+
+    Transloadit *transloadit = [[Transloadit alloc] initWithKey:@"5ae6b9c0f10c11e594a0bfa14ca2ffe1" andSecret:@"a9d351b355bb47b21af79d89aa9d8a54a6f27a41"];
+    
+
+    
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"test" ofType:@"jpg"];
+
+    
+    NSData *img = [NSData dataWithContentsOfFile:path];
+    
+    XCTAssertTrue(path != nil);
+    
+    NSError *error;
+    
+//    if (img != nil) {
+//        [assembly addFile:img withError:&error];
+//        
+//        XCTAssertTrue(error == nil);
+//    }
+    
+    NSMutableArray<Step *> *steps = [[NSMutableArray alloc] init];
+    
+    Step *step1 = [[Step alloc] initWithKey:@"encode"];
+    [step1 setValue:@"/image/resize" forOption:@"robot"];
+    
+    // Add the step to the array
+    [steps addObject:step1];
+    
+    //MARK: We then create an Assembly Object with the steps and files
+    Assembly *TestAssemblyWithSteps = [[Assembly alloc] initWithSteps:steps andNumberOfFiles:1];
+    [TestAssemblyWithSteps addFile:[NSURL fileURLWithPath:path]];
+    
+    [TestAssemblyWithSteps setTemplate:[[Template alloc] initWithTemplateId:@""]];
+    [transloadit createAssembly:TestAssemblyWithSteps];
+    
+    transloadit.assemblyCompletionBlock = ^(NSDictionary* completionDictionary){
+        /*Invoking The Assebmly does NOT need to happen inside the completion block. However for sake of a small UI it is.
+         We do however need to add the URL to the Assembly object so that we do invoke it, it knows where to go.
+         */
+        [TestAssemblyWithSteps setUrlString:[completionDictionary valueForKey:@"assembly_ssl_url"]];
+        [transloadit invokeAssembly:TestAssemblyWithSteps];
+        
+        [transloadit checkAssembly:TestAssemblyWithSteps];
+    };
+    
+
+    transloadit.assemblyStatusBlock = ^(NSDictionary* completionDictionary){
+        /*Invoking The Assebmly does NOT need to happen inside the completion block. However for sake of a small UI it is.
+         We do however need to add the URL to the Assembly object so that we do invoke it, it knows where to go.
+         */
+        NSLog(@"%@", [completionDictionary description]);
+        
+        if ([completionDictionary[@"ok"] isEqualToString:@"ASSEMBLY_COMPLETED"]) {
+             XCTAssertTrue([[completionDictionary objectForKey:@"uploads"] isEqualToString:@"1"]);
+
+        }
+    };
+    
+    
+}
+
 
 - (void)tearDown
 {
@@ -26,10 +93,6 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
 
 @end
 
