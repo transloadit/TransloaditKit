ae2b07ae5c607a16aa21775a1a2c0e1e163b0ba7
Mark R. Masterson <mark@masterson.io>
2020-10-26 20:54:54 -0700

    update example

--- Example/Transloadit/ViewController.swift
+++ Example/Transloadit/ViewController.swift
@@ -10,24 +10,53 @@ import UIKit
 import Transloadit
 import TUSKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    let imagePicker = UIImagePickerController()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
         //Transloadit.shared.delegate = self
         
-        var newSteps: [String: Any] = [:]
-        newSteps["robot"] = "/image/resize"
-        newSteps["width"] = 75
-        let assembly: Assembly = Transloadit.shared.newAssembly()
-        
-        assembly.addStep(name: "resize", options: newSteps)
-        assembly.addFile(withPathURL: URL(string: "http://google.com")!)
-        assembly.save()
+        present(imagePicker, animated: true, completion: nil)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK: Picker Delegate
+    
+        
+       
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+            if #available(iOS 11.0, *) {
+                guard let imageURL = info[.imageURL] else {
+                    return
+                }
+                
+                
+                var newSteps: [String: Any] = [:]
+                newSteps["robot"] = "/image/resize"
+                newSteps["width"] = 75
+                let assembly: Assembly = Transloadit.shared.newAssembly()
+                
+                assembly.addStep(name: "resize", options: newSteps)
+                assembly.addFile(withPathURL: imageURL as! URL)
+                assembly.save()
+                
+                
+            }
+            
+            dismiss(animated: true) {
+            }
+        }
+    //
+        func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+            dismiss(animated: true, completion: nil)
+        }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
