d7ee49d80b334cea24f0a957e16045d10a39137b
Tjeerd <tjeerd@twinapps.co>
2021-10-13 11:09:51 +0200

    Starting rewrite

--- AssemblyResponse.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AssemblyResponse.swift
-//  FBSnapshotTestCase
-//
-//  Created by Mark Robert Masterson on 10/26/20.
-//
-
-import Foundation
---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TransloaditKit",
+    platforms: [.iOS(.v10), .macOS(.v10_10)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "TransloaditKit",
+            targets: ["TransloaditKit"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", branch: "rewrite/start")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "TransloaditKit",
+            dependencies: ["TUSKit"]),
+        .testTarget(
+            name: "TransloaditKitTests",
+            dependencies: ["TransloaditKit"]),
+    ]
+)
