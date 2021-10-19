970827c3759385c09354f4456e20ae815c66e9ac
Tjeerd <tjeerd@twinapps.co>
2021-10-19 13:00:34 +0200

    Lowered Swift tools inside Package.swift from 5.5 to 5.3 to accomodate Github CI.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", branch: "rewrite/start")
+        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", .branch("rewrite/start"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
