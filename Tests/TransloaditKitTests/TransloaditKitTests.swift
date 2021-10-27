824f42bc5710ed1436613ae7667b01bb71b86cb5
Tjeerd <tjeerd@twinapps.co>
2021-10-27 13:46:48 +0200

    Making Polling logic more robust. Improving tests

--- Tests/TransloaditKitTests/TransloaditKitTests.swift
+++ Tests/TransloaditKitTests/TransloaditKitTests.swift
@@ -72,41 +72,6 @@ final class TransloaditKitTests: XCTestCase {
         data = Data("Hello".utf8)
     }
     
-    func testCreateAssembly() {
-        let serverAssembly = Fixtures.makeAssembly()
-        Network.prepareAssemblyResponse(assembly: serverAssembly)
-        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
-        transloadit.createAssembly(steps: [resizeStep], completion: { result in
-            switch result {
-            case .success(let receivedAssembly):
-                XCTAssertEqual(serverAssembly, receivedAssembly)
-            case .failure:
-                XCTFail("Expected call to succeed")
-            }
-            
-            serverFinishedExpectation.fulfill()
-        })
-        
-        waitForExpectations(timeout: 3, handler: nil)
-    }
-    
-    func testCreateAssembly_and_UploadFile() throws {
-        let (files, serverAssembly) = try Network.prepareForUploadingFiles(data: data)
-        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
-        transloadit.createAssembly(steps: [resizeStep], andUpload: files, completion: { result in
-            switch result {
-            case .success(let receivedAssembly):
-                XCTAssertEqual(serverAssembly, receivedAssembly)
-            case .failure:
-                XCTFail("Expected call to succeed")
-            }
-            
-            serverFinishedExpectation.fulfill()
-        })
-        
-        waitForExpectations(timeout: 3, handler: nil)
-    }
-    
     func testTransloaditDoesntPollIfAssemblyFails() throws {
         XCTFail("Implement me")
     }
@@ -115,14 +80,26 @@ final class TransloaditKitTests: XCTestCase {
         XCTFail("Implement me")
     }
     
-    func testCanCancelLocalUploads() throws {
-//        transloadit.cancelUploadsFor(assembly.id)
-        XCTFail("Implement me")
-    }
     
     // MARK: - File uploading
     
-    func testStatusUploadingOfFiles() throws {
+    func testCreateAssembly_Without_Uploading() throws {
+        let serverAssembly = Fixtures.makeAssembly()
+        Network.prepareAssemblyResponse(assembly: serverAssembly)
+        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
+        transloadit.createAssembly(steps: [resizeStep]) { result in
+            switch result {
+            case .success:
+            serverFinishedExpectation.fulfill()
+            case .failure:
+                XCTFail("Creating an assembly should have succeeded")
+            }
+        }
+        
+        waitForExpectations(timeout: 3.0, handler: nil)
+    }
+    
+    func testCreatingAssembly_And_Uploading_Of_Files() throws {
         let numFiles = 2
         let finishedUploadExpectation = self.expectation(description: "Finished file upload")
         finishedUploadExpectation.expectedFulfillmentCount = numFiles
@@ -133,108 +110,64 @@ final class TransloaditKitTests: XCTestCase {
         fileDelegate.finishUploadExpectation = finishedUploadExpectation
         fileDelegate.startUploadExpectation = startedUploadsExpectation
         
-        
         let (files, serverAssembly) = try Network.prepareForUploadingFiles(data: data)
-        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
-        transloadit.createAssembly(steps: [resizeStep], andUpload: files, completion: { result in
+        
+        createAssembly(files, completed: { result in
+                        
             switch result {
             case .success(let receivedAssembly):
                 XCTAssertEqual(serverAssembly, receivedAssembly)
             case .failure:
                 XCTFail("Expected call to succeed")
             }
-            
-            serverFinishedExpectation.fulfill()
         })
         
-        waitForExpectations(timeout: 3, handler: nil)
+        wait(for: [startedUploadsExpectation, finishedUploadExpectation], timeout: 3)
 
         XCTAssertEqual(numFiles, fileDelegate.finishedUploads.count)
         XCTAssertEqual(numFiles, fileDelegate.startedUploads.count)
     }
+    
+    func testCanCancelLocalUploads() throws {
+//        transloadit.cancelUploadsFor(assembly.id)
+        XCTFail("Implement me")
+    }
    
     
     // MARK: - Polling
-                
-    func testPolling() throws {
-        
-        let retryCount = 1
-        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
-        let pollingExpectation = expectation(description: "Waiting for polling to be called")
-        pollingExpectation.expectedFulfillmentCount = retryCount + 1// preparing status gives two calls
-        let pollingStatusCompleteExpectation = expectation(description: "Waiting for polling status to be complete")
-        
-        let (files, serverAssembly) = try Network.prepareForUploadingFiles(data: data)
-        
-        var count = retryCount
-        Network.prepareForStatusChecks(assembly: serverAssembly) {
+    
+    private func prepareNetworkForStatusCheck(assemblyURL: URL, expectedStatus: AssemblyStatus.Status = .completed) {
+        var count = 1
+        Network.prepareForStatusChecks(assemblyURL: assemblyURL) {
             if count == 0 {
-                return Fixtures.makeAssemblyStatus(status: .completed)
+                return Fixtures.makeAssemblyStatus(status: expectedStatus)
             } else {
                 count -= 1
                 return Fixtures.makeAssemblyStatus(status: .uploading)
             }
         }
       
-        transloadit.createAssembly(steps: [resizeStep], andUpload: files, completion: { result in
-            switch result {
-            case .success(let receivedAssembly):
-                XCTAssertEqual(serverAssembly, receivedAssembly)
-            case .failure:
-                XCTFail("Expected call to succeed")
-            }
-            
-            serverFinishedExpectation.fulfill()
-        }).pollAssemblyStatus { result in
-            pollingExpectation.fulfill()
-            
-            switch result {
-            case .success(let status):
-                if status.status == .completed {
-                    pollingStatusCompleteExpectation.fulfill()
-                }
-            case .failure(let error):
-                XCTFail("Polling threw error \(error)")
-            }
-        }
-        
-        waitForExpectations(timeout: 8, handler: nil) // larger timeout time because of polling
+    }
+                
+    func testPolling() throws {
+        try poll(statusToTestAgainst: .completed)
     }
     
-    func testPollingStopsOnCancel() throws {
-        let expectedPollCounts = 2
-        var count = expectedPollCounts - 1
-        
-        try poll(expectedPollCount: expectedPollCounts, statusToTestAgainst: .canceled, status: {
-            if count == 0 {
-                return Fixtures.makeAssemblyStatus(status: .canceled)
-            } else {
-                count -= 1
-                return Fixtures.makeAssemblyStatus(status: .uploading)
-            }
-        })
+    func testPollingStopsOnCancelState() throws {
+        // It's one thing if polling stops when status is completed. But it needs to stop polling on errors and canceled actions
+        try poll(statusToTestAgainst: .canceled)
     }
     
-    func testPollingStopsOnAborted() throws {
-        let expectedPollCounts = 2
-        var count = expectedPollCounts - 1
-        
-        try poll(expectedPollCount: expectedPollCounts, statusToTestAgainst: .aborted, status: {
-            if count == 0 {
-                return Fixtures.makeAssemblyStatus(status: .aborted)
-            } else {
-                count -= 1
-                return Fixtures.makeAssemblyStatus(status: .uploading)
-            }
-        })
+    func testPollingStopsOnAbortedState() throws {
+        // It's one thing if polling stops when status is completed. But it needs to stop polling on errors and canceled actions
+        try poll(statusToTestAgainst: .aborted)
     }
                 
     
-    func poll(expectedPollCount: Int, statusToTestAgainst: AssemblyStatus.Status, status: @escaping () -> AssemblyStatus) throws {
-        // It's one thing if polling stops when status is completed. But it needs to stop polling on errors and canceled actions
-        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
-        let pollingExpectation = expectation(description: "Waiting for polling to be called")
-        pollingExpectation.expectedFulfillmentCount = expectedPollCount
+    func poll(statusToTestAgainst: AssemblyStatus.Status) throws {
+        let defaultPollingTime: Double = 3
+        let pollingExpectation = expectation(description: "Waiting for polling to be called twice")
+        pollingExpectation.expectedFulfillmentCount = 2 // The amount of calls required before a status check is finished (e.g. status is set to completed/canceled/aborted)
         let pollingStatusCompleteExpectation = expectation(description: "Waiting for polling status to be complete")
         
         let noMoreCallsExpectation = expectation(description: "This shouldn't be called")
@@ -242,41 +175,34 @@ final class TransloaditKitTests: XCTestCase {
         
         let (files, serverAssembly) = try Network.prepareForUploadingFiles(data: data)
         
-        Network.prepareForStatusChecks(assembly: serverAssembly, status: status)
+        prepareNetworkForStatusCheck(assemblyURL: serverAssembly.url, expectedStatus: statusToTestAgainst)
         
         var isStatusCanceled = false
         
-        transloadit.createAssembly(steps: [resizeStep], andUpload: files, completion: { result in
-            switch result {
-            case .success(let receivedAssembly):
-                XCTAssertEqual(serverAssembly, receivedAssembly)
-            case .failure:
-                XCTFail("Expected call to succeed")
-            }
-            
-            serverFinishedExpectation.fulfill()
-        }).pollAssemblyStatus { result in
-            pollingExpectation.fulfill()
-            
-            switch result {
-            case .success(let status):
-                if status.status == statusToTestAgainst {
-                    pollingStatusCompleteExpectation.fulfill()
-                    // Now make sure that canceled isn't called any more
-                    if isStatusCanceled {
-                        noMoreCallsExpectation.fulfill()
+        createAssembly(files)
+            .pollAssemblyStatus { result in
+                
+                pollingExpectation.fulfill()
+                
+                switch result {
+                case .success(let status):
+                    if status.status == statusToTestAgainst {
+                        pollingStatusCompleteExpectation.fulfill()
+                        // Now make sure that canceled isn't called any more
+                        if isStatusCanceled {
+                            noMoreCallsExpectation.fulfill()
+                        }
+                        isStatusCanceled = true
                     }
-                    isStatusCanceled = true
+                case .failure(let error):
+                    XCTFail("Polling threw error \(error)")
                 }
-            case .failure(let error):
-                XCTFail("Polling threw error \(error)")
             }
-        }
         
-        wait(for: [serverFinishedExpectation, pollingExpectation, pollingStatusCompleteExpectation], timeout:  3)
+        wait(for: [pollingExpectation, pollingStatusCompleteExpectation], timeout:  defaultPollingTime * 2 + 1)
         
         // We wait to make sure polling doesnt keep calling
-        wait(for: [noMoreCallsExpectation], timeout: 9)
+        wait(for: [noMoreCallsExpectation], timeout: defaultPollingTime * 3)
     }
     
     func testPollingSameFilesMultipleTimesShouldNotBreak() throws {
@@ -284,6 +210,27 @@ final class TransloaditKitTests: XCTestCase {
         try testPolling()
         try testPolling()
     }
+    
+    // MARK: - Creating Assembly util
+    
+    @discardableResult
+    private func createAssembly(_ files: [URL]) -> TransloaditPoller {
+        return createAssembly(files, completed: { _ in })
+    }
+    
+    @discardableResult
+    private func createAssembly(_ files: [URL], completed: @escaping (Result<Assembly, TransloaditError>) -> Void) -> TransloaditPoller {
+        let serverFinishedExpectation = expectation(description: "Waiting for createAssembly to be called")
+        let poller = transloadit.createAssembly(steps: [resizeStep], andUpload: files, completion: { result in
+            serverFinishedExpectation.fulfill()
+            
+            completed(result)
+        })
+        
+        wait(for: [serverFinishedExpectation], timeout: 3)
+        return poller
+    }
+    
         
 }
 
@@ -325,6 +272,7 @@ enum Files {
 
 enum Network {
     
+    @discardableResult
     static func prepareForUploadingFiles(data: Data) throws -> ([URL], Assembly) {
         let files = try Files.storeFiles(data: data, 2)
         
@@ -380,8 +328,7 @@ enum Network {
         
     }
     
-    static func prepareForStatusChecks(assembly: Assembly, status: @escaping () -> AssemblyStatus) {
-        let statusURL = assembly.url
+    static func prepareForStatusChecks(assemblyURL statusURL: URL, status: @escaping () -> AssemblyStatus) {
         
         MockURLProtocol.prepareResponse(for: statusURL, method: "GET") { _ in
             let assemblyStatus: AssemblyStatus = status()
