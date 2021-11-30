45a91be115384e972a003d4d226c9e915d313a21
Tjeerd <tjeerd@twinapps.co>
2021-11-30 09:06:50 +0100

    Pointing to remote TUSKit again

--- Package.swift
+++ Package.swift
@@ -14,9 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "TUSKit", url: "../TUSKit", .branch("context"))
-//        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", .branch("context"))
+        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", .branch("context"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
