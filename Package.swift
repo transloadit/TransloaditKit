9fcf3b667acebb9b85c289c331e40a4129d010e2
Elvirion Antersijn <e.antersijn@gmail.com>
2022-03-30 15:21:06 +0200

    Update TUSKit dependency with released version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", .branch("rewrite/start"))
+        .package(name: "TUSKit", url: "https://github.com/tus/TUSKit", from: "3.1.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
